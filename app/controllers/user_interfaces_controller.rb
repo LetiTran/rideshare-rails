2635d1309eab00c6394b57a4cd23699f2480539e
LetiTran <leticiastran@gmail.com>
2018-04-03 14:20:28 -0700

    Started changing the home page.

--- app/controllers/user_interfaces_controller.rb
+++ app/controllers/user_interfaces_controller.rb
@@ -1,4 +1,5 @@
 class UserInterfacesController < ApplicationController
   def index
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,5 +44,7 @@ Rails.application.routes.draw do
 
   delete 'trips/:id', to: 'trips#destroy'
 
+  # root '#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
