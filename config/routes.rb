918a99524befc48a8d9a12a9e0501b0178d86df4
Steffnay <steffannbrown@gmail.com>
2018-04-03 11:26:15 -0700

    updated controllers, models, and routes

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :trips
+  resources :passengers
+  resources :drivers
+
+  root 'trips#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
