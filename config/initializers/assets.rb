faed1914c48aee0b9285e428bba1f80dabd8770c
LetiTran <leticiastran@gmail.com>
2018-04-06 15:02:00 -0700

    Refactored assets files.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( drivers.css )
-Rails.application.config.assets.precompile += %w( passangers.css )
-Rails.application.config.assets.precompile += %w( trips.css )
+Rails.application.config.assets.precompile += %w( show.css )
